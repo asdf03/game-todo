72b89254cb11a887cc13b0f32c622423b31860be
masaya nakatsuka <nmsuteado2@yahoo.co.jp>
2024-03-13 08:59:02 +0900

    フロント側のみ:新しいtodoの追加機能を作成

--- gt-front/src/components/TodoList.vue
+++ gt-front/src/components/TodoList.vue
@@ -7,33 +7,55 @@
           {{ todo.title }}
         </li>
       </ul>
+      <div v-if="!addingTodo" @click="addTodo" class="add-todo-placeholder">
+        Click here to add a new todo...
+      </div>
+      <!-- 新しいTodoの入力フィールド -->
+      <input v-if="addingTodo" v-model="newTodoTitle" @keyup.enter="saveTodo" placeholder="Enter new todo title" class="todo-input" ref="newTodoInput" />
     </div>
   </div>
 </template>
 
-<script>
-import axios from 'axios';
+<script setup>
+import { ref, nextTick } from 'vue';
 
-export default {
-  data() {
-    return {
-      todos: []
-    };
-  },
-  mounted() {
-    this.fetchTodos();
-  },
-  methods: {
-    async fetchTodos() {
-      try {
-        const response = await axios.get('http://aaa/todos');
-        this.todos = response.data;
-      } catch (error) {
-        console.error('Error fetching todos:', error);
-      }
-    }
-  }
+const todos = ref([
+  { id: 1, title: "買い物に行く" },
+  { id: 2, title: "レポートを書く" },
+  { id: 3, title: "運動する" }
+]);
+
+const addingTodo = ref(false);
+const newTodoTitle = ref("");
+
+const addTodo = () => {
+  addingTodo.value = true;
+  nextTick(() => {
+    document.querySelector('.todo-input').focus();
+  });
+};
+
+const saveTodo = () => {
+  if (!newTodoTitle.value.trim()) return;
+  const newTodo = {
+    id: todos.value.length + 1, // 単純化のためIDを現在の長さ+1とする
+    title: newTodoTitle.value
+  };
+  todos.value.push(newTodo);
+  newTodoTitle.value = "";
+  addingTodo.value = false;
 };
 </script>
 
-<style></style>
+<style>
+.add-todo-placeholder {
+  cursor: pointer;
+  padding: 10px;
+  background-color: #f0f0f0;
+}
+.todo-input {
+  width: 100%;
+  padding: 10px;
+  margin-top: 10px;
+}
+</style>
